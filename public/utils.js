c79cba61f3c2f1a25c97514badb5b36f30047f68
bcandrews4-ctrl <bcandrews4@gmail.com>
2025-10-01 12:56:31 +0800

    Add files via upload

---
+++ public/utils.js
@@ -0,0 +1,8 @@
+
+function el(id){ return document.getElementById(id); }
+function fmt(s){
+  s = Math.max(0, Math.floor(s||0));
+  const m = Math.floor(s/60);
+  const ss = String(s%60).padStart(2,'0');
+  return `${m}:${ss}`;
+}
